4ea09d772454ef6f0ed787cabe7a4af4c2a814bd
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-03-22 09:30:20 -0300

    Ajustes css

---
+++ src/components/card.tsx
@@ -0,0 +1,61 @@
+import { Minus, Plus, ShoppingCart } from "@phosphor-icons/react";
+import { useState } from "react";
+
+interface CardProps {
+  coffee: {
+    id: string;
+    title: string;
+    description: string;
+    tags: string[];
+    price: number;
+    image: string;
+  };
+}
+
+export function Card({ coffee }: CardProps) {
+  const [quantity] = useState(1);
+
+  return (
+    <div className="flex flex-col items-center w-64 px-6 pb-5 bg-base-card rounded-tl-md rounded-tr-[36px] rounded-bl-[36px] rounded-br-md">
+      <img src={coffee.image} alt="" className="-mt-5 mb-3" />
+      <div className="flex items-center gap-1 mb-4">
+        {coffee.tags.map((tag) => {
+          return (
+            <span className="px-2 py-1 rounded-[100px] bg-yellow-light text-yellow-dark text-[10px] font-bold uppercase">
+              {tag}
+            </span>
+          );
+        })}
+      </div>
+      <h3 className="font-primary text-base-subtitle text-xl mb-1">
+        {coffee.title}
+      </h3>
+      <span className="text-base-label text-sm text-center mb-8 min-h-[60px] max-h-[60px] line-clamp-3">
+        {coffee.description}
+      </span>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-1 items-baseline">
+          <span className="text-sm">R$</span>
+          <span className="text-2xl font-primary font-extrabold">
+            {coffee.price.toFixed(2)}
+          </span>
+        </div>
+
+        <div className="flex gap-2">
+          <div className="flex items-center gap-2 p-2 rounded-md bg-base-button">
+            <button>
+              <Minus size={14} className="text-purple" />
+            </button>
+            <span className="text-base-title">{quantity}</span>
+            <button>
+              <Plus size={14} className="text-purple" />
+            </button>
+          </div>
+          <button className="bg-purple-dark p-2 rounded-md">
+            <ShoppingCart size={22} weight="fill" className="text-base-card" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,18 +4,24 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 bg-base-background px-48 flex justify-between items-center h-28">
-      <Link to="/">
-        <img src={logo} alt="Logo Coffe Delivery" />
-      </Link>
-      <div className="flex gap-3">
-        <div className="flex items-center gap-1 p-2 rounded-md bg-purple-light">
-          <MapPin size={22} weight="fill" className="text-purple" />
-          <span className="text-purple-dark text-sm">Orleans, SC</span>
+    <header className="fixed top-0 left-0 right-0 z-10 bg-base-background bg-opacity-95 ">
+      <div className="max-w-6xl m-auto flex justify-between items-center h-28">
+        <Link to="/">
+          <img src={logo} alt="Logo Coffe Delivery" />
+        </Link>
+        <div className="flex gap-3">
+          <div className="flex items-center gap-1 p-2 rounded-md bg-purple-light">
+            <MapPin size={22} weight="fill" className="text-purple" />
+            <span className="text-purple-dark text-sm">Orleans, SC</span>
+          </div>
+          <a className="flex items-center p-2 rounded-md bg-yellow-light">
+            <ShoppingCart
+              size={22}
+              weight="fill"
+              className="text-yellow-dark"
+            />
+          </a>
         </div>
-        <a className="flex items-center p-2 rounded-md bg-yellow-light">
-          <ShoppingCart size={22} weight="fill" className="text-yellow-dark" />
-        </a>
       </div>
     </header>
   );
